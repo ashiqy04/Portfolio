d01ed17b94b9c81ce8eb821314557e54c9772f60
ashiqy04 <ashiqy04@gmail.com>
2023-10-05 20:40:51 +0530

    transition in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,28 +17,28 @@ const Navbar = () => {
             </div>
 
             {/*Menu*/}
-            <ul className='hidden md:flex'>
-                <li>
+            <ul className='hidden md:flex '>
+                <li className='hover:scale-110 duration-500 hover:-translate-y-1 transition-transform cursor-pointer'>
                     <Link to="home" smooth={true} duration={500}>
                         Home
                     </Link>
                 </li>
-                <li>
+                <li className='hover:scale-110 duration-500 hover:-translate-y-1 transition-transform cursor-pointer'>
                     <Link to="about" smooth={true} duration={500}>
                         About
                     </Link>
                 </li>
-                <li>
+                <li className='hover:scale-110 duration-500 hover:-translate-y-1 transition-transform cursor-pointer'>
                     <Link to="skills" smooth={true} duration={500}>
                         Skills
                     </Link>
                 </li>
-                <li>
+                <li className='hover:scale-110 duration-500 hover:-translate-y-1 transition-transform cursor-pointer'>
                     <Link to="work" smooth={true} duration={500}>
                         Work
                     </Link>
                 </li>
-                <li>
+                <li className='hover:scale-110 duration-500 hover:-translate-y-1 transition-transform cursor-pointer'>
                     <Link to="contact" smooth={true} duration={500}>
                         Contact
                     </Link>
