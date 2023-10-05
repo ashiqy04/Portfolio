7534532ca775a4bf22523f025d0607e5b312a449
ashiqy04 <ashiqy04@gmail.com>
2023-10-05 18:46:43 +0530

    TypeWriter effect in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
 import { Link } from 'react-scroll'
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
   return (
@@ -11,7 +12,20 @@ const Home = () => {
 
         <p className='text-pink-600'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Ashiq Y</h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Software Developer</h2>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a
+          <Typewriter options={{
+            autoStart: true,
+            loop: true,
+          }}
+
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Software Developer")
+                .pauseFor(2000)
+                .deleteAll()
+                .start();
+            }} />
+        </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a Full Stack Developer proficient in Java Spring Boot for robust backend solutions and React.js for dynamic front-end development. Proficient in various technologies including databases, version control, and testing frameworks. Passionate about crafting seamless user experiences and scalable, efficient systems.</p>
         <div>
           <Link to="work" smooth={true} duration={500}>
