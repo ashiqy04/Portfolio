7c5a21b4997283d56534722a75cac2961990a9af
ashiqy04 <ashiqy04@gmail.com>
2023-10-07 19:23:02 +0530

    added padding top

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4 pt-24'>
         <form method='POST' action='https://getform.io/f/5864c35d-645f-42ef-b6ab-7a7872d12c92' className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-400 text-gray-300'>Contact</p>
