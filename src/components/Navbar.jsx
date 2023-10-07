3335f2671fb396cb7a5303689c0210a020a68c3f
ashiqy04 <ashiqy04@gmail.com>
2023-10-07 18:31:52 +0530

    resume link changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -106,7 +106,7 @@ const Navbar = () => {
 
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
                         <a className='flex justify-between items-center w-full text-gray-300'
-                            href="https://drive.google.com/file/d/1kILVlMbRqsb6DYMMIIs7W-CjJGE-QvYK/view?usp=drive_link">
+                            href="https://drive.google.com/file/d/1mEM2EVRnKPXGniBb78nwFIL0eLRx2moO/view?usp=drive_link">
                             Resume <BsFillPersonLinesFill size={30} />
                         </a>
                     </li>
